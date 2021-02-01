23cf202d49113811f9d1811538881a956b8c976c
owenmoogk <owenmoogk@gmail.com>
2021-02-01 13:55:48 -0500

    more dynamic

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ INITCOLOR = 'blue'
 SWAPCOLOR = "red"
 FINALCOLOR = "#90EE90"
 animationSpeed = 5
+numOfBars = 150
 
 function resetArray(){
     numberset = []
     txt = ""
-    for (let i = 0; i < 100; i++){
+    for (let i = 0; i < numOfBars; i++){
         numberset.push(randomIntFromInterval(5, 700))
         txt += "<div class='array-bar' style='height:"+numberset[i]+"px'></div>"
     }
