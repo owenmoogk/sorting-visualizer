1557676e49b79f29126384da363f413ae361e51b
owenmoogk <owenmoogk@gmail.com>
2021-02-02 15:20:43 -0500

    disabled buttons while running

--- main.js
+++ main.js
@@ -22,13 +22,31 @@ function doFinishAnimation(){
         setTimeout(() => {
             arrayBars[i].style.backgroundColor = FINALCOLOR
         }, i * animationSpeed * 2)
+        setTimeout(() => {
+            if (i == arrayBars.length - 1){
+                enableButtons()
+            }
+        }, i * animationSpeed * 2)
     }
+}
+
+function enableButtons(){
+    document.getElementById("multiplier").disabled = false
+    document.getElementById("sorting-algorithm").disabled = false
+    document.getElementById("go").disabled = false
+    document.getElementById("reset").disabled = false
 }
 
 function doSort(){
     algorithm = document.getElementById("sorting-algorithm").value
     multiplier = document.getElementById("multiplier").value
     animationSpeed = defaultAnimationSpeed * (1/multiplier)
+
+
+    document.getElementById("multiplier").disabled = true
+    document.getElementById("sorting-algorithm").disabled = true
+    document.getElementById("go").disabled = true
+    document.getElementById("reset").disabled = true
 
     if (algorithm == "merge"){doMergeSort()}
     else if (algorithm == "bubble"){doBubbleSort()}
